01926e34be236b6a73bfdc8039c0473058791c61
hayder <hayderbd4290@gmail.com>
2024-10-30 17:02:21 +0600

    spinner loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import './App.css'
 import NavBar from './Components/NavBar'
 import Footer from './Components/Footer'
+import { CirclesWithBar } from 'react-loader-spinner'
 
 function App() {
 
-
+  const navigation = useNavigation()
+  console.log(navigation)
   return (
     <>
       <NavBar></NavBar>
-       <Outlet></Outlet>
+      {
+        navigation.state==="loading"? <div 
+        className="flex justify-center">
+          <CirclesWithBar
+        height="100"
+        width="100"
+        color="#4fa94d"
+        outerCircleColor="#4fa94d"
+        innerCircleColor="#4fa94d"
+        barColor="#4fa94d"
+        ariaLabel="circles-with-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        /> 
+        </div>:
+        <Outlet></Outlet>
+      }
+       
       <Footer></Footer>
     </>
   )
